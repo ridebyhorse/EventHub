1aaa1dc52d7615893bc0cb270e6fe90a08b9d49c
Ivan Semikin <i.semikin.94@icloud.com>
2024-11-18 19:58:38 +0100

    Created empty EventsScreenView with new directory

---
+++ EventHub/Screens/EventsScreen/EventsScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  EventsScreenView.swift
+//  EventHub
+//
+//  Created by Иван Семикин on 18/11/2024.
+//
+
+import SwiftUI
+
+struct EventsScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventsScreenView()
+}
