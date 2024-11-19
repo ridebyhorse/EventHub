962c05665a12497e0f6eecbb4f025bdb707186d3
Ivan Semikin <i.semikin.94@icloud.com>
2024-11-19 18:36:25 +0100

    Created EventView with new directory

---
+++ EventHub/Screens/EventsScreen/Views/EventView.swift
@@ -0,0 +1,18 @@
+//
+//  EventView.swift
+//  EventHub
+//
+//  Created by Иван Семикин on 19/11/2024.
+//
+
+import SwiftUI
+
+struct EventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventView()
+}
